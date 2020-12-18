5b1a40768ff9edb3579ed553bdc6fc09d003c9aa
Sujit Mahavarkar <s.mahavarkar@gmail.com>
2020-12-18 20:13:30 +0700

    adds a solution for addition

---
+++ Intro/add.js
@@ -0,0 +1,9 @@
+//Write a function that returns the sum of two numbers.
+
+//old javascript
+function add(param1,param2) {
+	return param1+param2;
+}
+
+//arrow function
+const add = (param1,param2) => param1+param2
