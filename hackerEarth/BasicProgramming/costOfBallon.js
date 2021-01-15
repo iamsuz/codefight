e241af06d1ee8448a76f220054149a31af1187a1
Sujit M <s.mahavarkar@gmail.com>
2021-01-15 18:00:09 +0700

    restructure the file system

---
+++ hackerEarth/BasicProgramming/costOfBallon.js
@@ -0,0 +1,8 @@
+const main = (input) =>{
+	var inArr = input.split('\n');
+    var testCase = inArr[0];
+    let costs = inArr[1].split(' ');
+}
+
+
+console.log(main("2\n9 6\n10\n1 1\n1 1\n\n0 1\n0 0\n0 1\n0 0\n0 1\n0 1\n1 1\n0 0\n1 9\n10\n0 1\n0 0\n0 0\n0 1\n1 0\n0 1\n0 1\n0 0\n0 1\n0 0"))
--- hackerEarth/README.md
+++
@@ -1,4 +0,0 @@
-# codefight
-Descriptive solutions for code fight
-
-You can run all the files with the input provided by the codefights
